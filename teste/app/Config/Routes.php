07712f5dedda956ceeec8ce377becd2237d0b2d2
Ariell Cannal <ariell@cannal.com.br>
2025-09-06 17:15:01 -0300

    fix: reverte controlador e garante model obrigatório

--- teste/app/Config/Routes.php
+++ teste/app/Config/Routes.php
@@ -1,8 +1,19 @@
 <?php
 
+namespace Config;
+
+use CodeIgniter\Config\Services;
 use CodeIgniter\Router\RouteCollection;
 
 /**
+ * Rotas principais da aplicação de testes.
+ *
  * @var RouteCollection $routes
  */
+$routes = Services::routes();
+
+// Rota padrão para a página inicial
 $routes->get('/', 'Home::index');
+
+return $routes;
+
